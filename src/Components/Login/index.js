612d6c4a06c6481213aa0c3fad02ca7a32ad0f52
chanhenry54 <hechan@ucsd.edu>
2020-06-20 16:51:24 -0700

    started working on login

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -3,9 +3,24 @@ import React from "react";
 import "./style.css";
 
 const Login = () => {
-    return(
-        <div className="login">
-            Login 
+    return (
+        <div className="login-container">
+            <div className="login-header">
+                <h1>Log In</h1>
+            </div>
+            <div className="input-container">
+                <form onSubmit={handleLogin}>
+                    <div className="input-field">
+                        <input name="email" type="email" placeholder="Email" />
+                    </div>
+                    <div className="input-field">
+                        <input name="password" type="password" placeholder="Password" />
+                    </div>
+                    <div className="input-button">
+                        <button type="submit">Log In</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
