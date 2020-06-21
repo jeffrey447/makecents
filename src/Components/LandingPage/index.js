ed55bbfc1649927b9db44bd13b7eb1a5c2b31612
Steven Steiner <steets250@gmail.com>
2020-06-20 19:59:59 -0700

    Frontend Cleaning + Updates

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -1,28 +1,40 @@
 import React from "react";
-import Logo from '../../Assets/Logo.png';
-import { Button } from 'antd';
+import { Button } from "antd";
 
+import Logo from "../../Assets/logo.svg";
+import Banner from "../../Assets/landing-image.jpg";
 
 import "./style.less";
 
 const LandingPage = () => {
-    return (
-        <div className="landing-page">
-            <div className="header">
-                
-                <img src={Logo} alt="Logo" className="logo" />
-                <div className="buttons">
-                    <Button className="login" size="large">Login</Button>
-                    <Button className="sign-up" size="large">Sign Up</Button>
-                </div>
-            </div>
-            <div>
-                <p className="paragraph">We live in a world where black communities live in fear because of injustice,  state-sanctioned violence and anti-Black racism. Only together and through action are we able to make a change.
-Make Cents is a platform that allows users regardless of age, sex, and orientation help eradicate all this injustice. Users are able to automatically donate spare change to various origanizations that support black communities fight injustice.
-Sign up now and donate your spare change to an issue that makes sense.</p>
-            </div>
+  return (
+    <div className="landing-page">
+      <div className="header">
+        <img src={Logo} alt="Make Cents Logo" className="logo" />
+        <div className="buttons">
+          <Button className="login" size="large">
+            Login
+          </Button>
+          <Button className="sign-up" size="large">
+            Sign Up
+          </Button>
         </div>
-    );
+      </div>
+      <div className="content">
+        <p className="paragraph">
+          We live in a world where black communities live in fear because of
+          injustice, state-sanctioned violence and anti-Black racism. Only
+          together and through action are we able to make a change. Make Cents
+          is a platform that allows users regardless of age, sex, and
+          orientation help eradicate all this injustice. Users are able to
+          automatically donate spare change to various origanizations that
+          support black communities fight injustice. Sign up now and donate your
+          spare change to an issue that makes sense.
+        </p>
+        <img className="banner" src={Banner} alt="Banner" />
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import WebFont from "webfontloader";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Dashboard from "./Components/Dashboard";
+import History from "./Components/History";
+import LandingPage from "./Components/LandingPage";
+import Login from "./Components/Login";
+import Settings from "./Components/Settings";
+import Signup from "./Components/Signup";
+
+import "./index.css";
+
+WebFont.load({
+  google: {
+    families: ["Montserrat:100,200,300,400,500,600,700,800,900"],
+  },
+});
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route exact path="/history">
+          <History />
+        </Route>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/settings">
+          <Settings />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
