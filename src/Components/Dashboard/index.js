b84b4219be70669df3f55c2c4da209b1a318c82e
Jeffrey Ha <jha@ucsd.edu>
2020-06-21 03:56:12 -0700

    added auth checking

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import fbase from '../../firebase';
 
 import "./style.less";
 
-const Dashboard = () => {
-  return <div className="dashboard">Dashboard</div>;
+const Dashboard = ({ history }) => {
+    const [user, loading, error] = useAuthState(fbase.auth);
+
+    if (loading) {
+        // can replace?
+        return (
+            <div>
+                <p>Loading user...</p>
+            </div>
+        );
+    }
+
+    if (error) {
+        // can replace?
+        return (
+            <div>
+                <p>Error: <b>{error}</b></p>
+            </div>
+        );
+    }
+
+    if (!user) {
+        // not logged in so
+        history.push('/');
+
+        // we have to return something so we'll return an empty page.
+        return (
+            <div></div>
+        );
+    } else {
+        // user is logged in so display the page.
+
+        return <div className="dashboard">Dashboard</div>;
+    }
 };
 
 export default Dashboard;
