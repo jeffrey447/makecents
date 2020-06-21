3b5acee4c4857a197aaa49338b09603d75d1c762
Jeffrey Ha <jha@ucsd.edu>
2020-06-21 03:56:34 -0700

    added auth checking & bank account connecting

--- src/Components/Settings/index.js
+++ src/Components/Settings/index.js
@@ -1,63 +1,110 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import fbase from '../../firebase';
+
 import { PlaidLink } from 'react-plaid-link';
 
 import { Button } from "antd";
 import { Link } from "react-router-dom";
 
 import Logo from "../../Assets/logo.svg";
-import Banner from "../../Assets/landing-image.jpg";
-
 
 import "./style.less";
 
+const Settings = ({ history }) => {
+    const [user, loading, error] = useAuthState(fbase.auth);
 
+    const onSuccess = (token, metadata) => {
+        //console.log(token);
+        //console.log(metadata);
 
-const Settings = () => {
-  const onSuccess = (token, metadata) => {
-    console.log(token);
-    console.log(metadata);
-  }
-  return (
+        /*
+            if you wanted to remove their bank, you can just provide a
+                null token and null name. it's equivalent.
+        */
+        axios.post(`http://localhost:5000/api/users/${user.uid}/add_bank`, {
+            token: token,
+            name: metadata.institution.name
+        }).then((data) => {
+            if (data.success) {
+                // yeet
+            } else {
+                console.log(data.error);
+            }
+        }).catch((error) => {
+            console.log(error);
+        })
+    };
     
-    <div className="settings">
-      <div className="header">
-        <img src={Logo} alt="Make Cents Logo" className="logo" />
-        <div className="buttons">
-          <Link to="/dashboard">
-            <Button className="dashboard-button" size="large">
-              Dashboard
-            </Button>
-          </Link>
-          <Link to="/about">
-            <Button className="about-button" size="large">
-              About
-            </Button>
-          </Link>
-          <Link to="/settings">
-            <Button className="settings-button" size="large">
-              Settings
-            </Button>
-          </Link>
-          <Link to="/">
-            <Button className="log-out" size="large">
-              Log Out
-            </Button>
-          </Link>
+    if (loading) {
+        // can replace?
+        return (
+            <div>
+                <p>Loading user...</p>
+            </div>
+        );
+    }
 
-        </div>
-      </div>
-      <PlaidLink
-        clientName="Makes Cents"
-        env="sandbox"
-        product={['auth', 'transactions']}
-        publicKey={process.env.REACT_APP_PLAID_PUBLIC_KEY}
-        onSuccess={onSuccess}
-      >
-        Connect a bank account
-      </PlaidLink>
-    </div>
+    if (error) {
+        // can replace?
+        return (
+            <div>
+                <p>Error: <b>{error}</b></p>
+            </div>
+        );
+    }
 
-  )
+    if (!user) {
+        // not logged in so
+        history.push('/');
+
+        // we have to return something so we'll return an empty page.
+        return (
+            <div></div>
+        );
+    } else {
+        // user is logged in so display the page.
+        
+        return (
+            <div className="settings">
+                <div className="header">
+                    <img src={Logo} alt="Make Cents Logo" className="logo" />
+                    <div className="buttons">
+                    <Link to="/dashboard">
+                        <Button className="dashboard-button" size="large">
+                        Dashboard
+                        </Button>
+                    </Link>
+                    <Link to="/about">
+                        <Button className="about-button" size="large">
+                        About
+                        </Button>
+                    </Link>
+                    <Link to="/settings">
+                        <Button className="settings-button" size="large">
+                        Settings
+                        </Button>
+                    </Link>
+                    <Link to="/">
+                        <Button className="log-out" size="large">
+                        Log Out
+                        </Button>
+                    </Link>
+                    </div>
+                </div>
+                <PlaidLink
+                    clientName="Makes Cents"
+                    env="sandbox"
+                    product={['auth', 'transactions']}
+                    publicKey={process.env.REACT_APP_PLAID_PUBLIC_KEY}
+                    onSuccess={onSuccess}
+                >
+                    Connect a bank account
+                </PlaidLink>
+            </div>
+        );
+    }
 };
 
 export default Settings;
