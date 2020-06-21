c0419bec0077f08cfd1b5f16c88380bd9c2a529a
bamkho4 <phanmichael77@gmail.com>
2020-06-21 00:12:05 -0700

    Header for settings page

--- src/Components/Settings/index.js
+++ src/Components/Settings/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { PlaidLink } from 'react-plaid-link';
+
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+
+import Logo from "../../Assets/logo.svg";
+import Banner from "../../Assets/landing-image.jpg";
+
+
 import "./style.less";
+
+
 
 const Settings = () => {
   const onSuccess = (token, metadata) => {
@@ -8,7 +18,34 @@ const Settings = () => {
     console.log(metadata);
   }
   return (
+    
     <div className="settings">
+      <div className="header">
+        <img src={Logo} alt="Make Cents Logo" className="logo" />
+        <div className="buttons">
+          <Link to="/dashboard">
+            <Button className="dashboard-button" size="large">
+              Dashboard
+            </Button>
+          </Link>
+          <Link to="/about">
+            <Button className="about-button" size="large">
+              About
+            </Button>
+          </Link>
+          <Link to="/settings">
+            <Button className="settings-button" size="large">
+              Settings
+            </Button>
+          </Link>
+          <Link to="/">
+            <Button className="log-out" size="large">
+              Log Out
+            </Button>
+          </Link>
+
+        </div>
+      </div>
       <PlaidLink
         clientName="Makes Cents"
         env="sandbox"
@@ -19,6 +56,7 @@ const Settings = () => {
         Connect a bank account
       </PlaidLink>
     </div>
+
   )
 };
 
