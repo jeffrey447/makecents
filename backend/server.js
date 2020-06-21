3bf881075d1e4c698584de62a8d2622ca5626986
Jeffrey Ha <jha@ucsd.edu>
2020-06-21 16:01:52 -0700

    cors attempt fix?

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ app.use(bodyParser.json());
 
 // to avoid an CORS issue
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    //http://localhost:3000
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
