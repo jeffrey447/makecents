a2cff3a7f5899bfd6c67c6ac9beabe40d1b3263e
Justin Nguyen <justnguyen1@gmail.com>
2020-06-20 20:14:22 -0700

    landing page

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "antd";
+import { Link } from "react-router-dom";
 
 import Logo from "../../Assets/logo.svg";
 import Banner from "../../Assets/landing-image.jpg";
@@ -12,25 +13,37 @@ const LandingPage = () => {
       <div className="header">
         <img src={Logo} alt="Make Cents Logo" className="logo" />
         <div className="buttons">
-          <Button className="login" size="large">
-            Login
-          </Button>
-          <Button className="sign-up" size="large">
-            Sign Up
-          </Button>
+          <Link to="/login">
+            <Button className="login" size="large">
+              Login
+            </Button>
+          </Link>
+          <Link to="/signup">
+            <Button className="sign-up" size="large">
+              Sign Up
+            </Button>
+          </Link>
+
         </div>
       </div>
       <div className="content">
-        <p className="paragraph">
-          We live in a world where black communities live in fear because of
-          injustice, state-sanctioned violence and anti-Black racism. Only
-          together and through action are we able to make a change. Make Cents
-          is a platform that allows users regardless of age, sex, and
-          orientation help eradicate all this injustice. Users are able to
-          automatically donate spare change to various origanizations that
-          support black communities fight injustice. Sign up now and donate your
-          spare change to an issue that makes sense.
-        </p>
+        <div className="paragraph-button">
+          <p className="paragraph">
+            We live in a world where black communities live in fear because of
+            injustice, state-sanctioned violence and anti-Black racism. Only
+            together and through action are we able to make a change. <br></br><br></br>Make Cents
+            is a platform that allows users regardless of age, sex, and
+            orientation help eradicate all this injustice. Users are able to
+            automatically donate spare change to various origanizations that
+            support black communities fight injustice. <br></br><br></br>Sign up now and donate your
+            spare change to an issue that makes sense.
+          </p>
+          <Link to="/signup">
+            <Button className="sign-up" size="large">
+              Sign Up
+            </Button>
+          </Link>
+        </div>
         <img className="banner" src={Banner} alt="Banner" />
       </div>
     </div>
