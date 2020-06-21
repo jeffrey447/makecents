26936cc5e52aec18605ca2e1c321722079d0c40b
Justin Nguyen <justnguyen1@gmail.com>
2020-06-20 18:15:25 -0700

    text for landing page

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -12,9 +12,14 @@ const LandingPage = () => {
                 
                 <img src={Logo} alt="Logo" className="logo" />
                 <div className="buttons">
-                    <Button className="login">Login</Button>
-                    <Button className="sign-up">Sign Up</Button>
+                    <Button className="login" size="large">Login</Button>
+                    <Button className="sign-up" size="large">Sign Up</Button>
                 </div>
+            </div>
+            <div>
+                <p className="paragraph">We live in a world where black communities live in fear because of injustice,  state-sanctioned violence and anti-Black racism. Only together and through action are we able to make a change.
+Make Cents is a platform that allows users regardless of age, sex, and orientation help eradicate all this injustice. Users are able to automatically donate spare change to various origanizations that support black communities fight injustice.
+Sign up now and donate your spare change to an issue that makes sense.</p>
             </div>
         </div>
     );
