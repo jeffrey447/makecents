191f93dc8f0ef3e1b58826d97f5dfaea155364ca
Justin Nguyen <justnguyen1@gmail.com>
2020-06-20 20:41:56 -0700

    login page

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,9 +1,70 @@
 import React from "react";
-
+import Logo from "../../Assets/logo.svg";
+import { Form, Input, Button, Checkbox } from 'antd';
 import "./style.less";
 
 const Login = () => {
-  return <div className="login">Login</div>;
-};
+  const onFinish = values => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div className="login">
+      <img src={Logo} alt="Make Cents Logo" className="logo" />
+      <div className="login-form">
+        <h1 className="login-title">Login</h1>
+        <Form
+        
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
