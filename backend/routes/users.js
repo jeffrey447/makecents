4db66fdf5a8f84504c78a0e0c010217cf250f005
Jeffrey Ha <jha@ucsd.edu>
2020-06-21 03:54:27 -0700

    added more api routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,55 +1,130 @@
 var express = require('express');
 var router = express.Router();
 
-var firebase = require('../firebase').default;
+var firebase = require('../firebase');
 var db = firebase.database();
 
-var getUserData = (uid) => {
-    var data = new Object();
+// helper function to receive the data of the user
+async function getUserData(uid) {
+    var snapshot = await db.ref(`/users/${uid}`).once('value');
+    
+    return snapshot.val();
+}
 
-    db.ref(`/users/${uid}`).once("value", (snapshot) => {
-        snapshot.forEach((c) => {
-           data[c.key] = c.val();
-        });
-    });
-
-    return data;
-};
-
-router.post('/:uid/transactions', (req, res) => {
+/*  USER DATA RELATED  */
+router.get('/:uid', async (req, res) => {
     var uid = req.params.uid;
-    var userData = getUserData(uid);
-    var transactions = userData.transactions || {};
+    var userData = await getUserData(uid);
 
-    console.log(transactions);
-    res.json({
-        success: true,
-        transactions: transactions
-    });
-});
-
-router.post('/:uid/card/:token/add', (req, res) => {
-    var uid = req.params.uid;
-    var token = req.params.token;
-
-    var userData = getUserData(uid);
-    var transactions = userData.transactions || {};
-
-    transactions[token] = {};
-
-    db.ref(`/users/${uid}`).update({
-        transactions: transactions
-    }).then(() => {
-        res.json({
-            success: true,
-            transactions: transactions
-        });
-    }).catch((error) => {
+    if (!userData) {
         res.json({
             success: false,
-            error: error
+            error: "User not found."
         });
-    });
+    } else {
+        res.json({
+            success: true,
+            user: userData
+        });
+    }
+});
+
+router.post('/:uid/add_bank', async (req, res) => {
+    var uid = req.params.uid;
+    var userData = await getUserData(uid);
+
+    // receive post data
+    var token = req.body.token;
+    var name = req.body.name; // bank name
+
+    if (!userData) {
+        res.json({
+            success: false,
+            error: "User not found."
+        });
+    } else {
+        try {
+            await db.ref(`/users/${uid}`).update({
+                bank: {
+                    token: token,
+                    name: name
+                }
+            });
+    
+            res.json({
+                success: true
+            });
+        } catch (error) {
+            res.json({
+                success: false,
+                error: error
+            });
+        }
+    }
+});
+
+/*  TRANSACTIONS  */
+router.get('/:uid/transactions', async (req, res) => {
+    var uid = req.params.uid;
+    var userData = await getUserData(uid);
+    
+    if (!userData) {
+        res.json({
+            success: false,
+            error: "User not found."
+        });
+    } else {
+        res.json({
+            success: true,
+            transactions: userData.transactions || {}
+        });
+    }
+});
+
+router.post('/:uid/card/:tid/add', async (req, res) => {
+    var uid = req.params.uid;
+    var tid = req.params.tid; // transaction id
+
+    // receive post data
+    var org = req.body.organization;
+    var date = req.body.date;
+    var amount = req.body.amount;
+    var donation = req.body.donation;
+
+    var userData = await getUserData(uid);
+    if (!userData) {
+        res.json({
+            success: false,
+            error: "User not found."
+        });
+    } else {
+        var totalDonation = (userData['total_donation'] || 0) + donation;
+        var bank = userData.bank || {};
+
+        transactions[tid] = {
+            organization: org,
+            date: date,
+            amount: amount,
+            donation: donation
+        };
+
+        try {
+            await db.ref(`/users/${uid}`).update({
+                transactions: transactions,
+                total_donation: totalDonation
+            });
+    
+            res.json({
+                success: true,
+                transactions: transactions
+            });
+        } catch (error) {
+            res.json({
+                success: false,
+                error: error
+            });
+        }
+    }
 });
 
 module.exports = router;
